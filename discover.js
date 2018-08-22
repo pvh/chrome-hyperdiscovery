02c6634f0982d80ce4ff05155ef7db24eb88fb76
Orion Henry <orion.henry@gmail.com>
2018-08-22 10:58:41 +0800

    basic patch

--- discover.js
+++ discover.js
@@ -2,7 +2,7 @@ var swarm = require("discovery-swarm")
 
 var sw = swarm({ utp: false })
 
-sw.listen(1010 + Math.floor(Math.random() * 10))
+sw.listen(1025 + Math.floor(Math.random() * 100))
 sw.join('chrome-app-test') // can be any id/name/hash
 
 sw.on("connection", function(connection) {
