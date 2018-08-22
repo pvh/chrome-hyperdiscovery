c8f8b661520dd920861fa5f6c78f8ddf0c5b2ee2
Peter van Hardenberg <pvh@pvh.ca>
2018-08-22 12:11:20 -0700

    wip on mdns

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
 var swarm = require('discovery-swarm')
 
 setTimeout( function() {
-  var sw = swarm()
+/*  var sw = swarm()
   sw.listen()
   sw.join('chrome-app-test')
   sw.on('connection', function (connection) {
     console.log(connection)
+  })*/
+  var mdns = require('multicast-dns')()
+
+mdns.on('warning', function (err) {
+  console.log(err.stack)
+})
+
+mdns.on('response', function (response) {
+  console.log('got a response packet:', response)
+})
+
+mdns.on('query', function (query) {
+  console.log('got a query packet:', query)
+
+  // iterate over all questions to check if we should respond
+  query.questions.forEach(function (q) {
+    if (q.type === 'A' && q.name === 'example.local') {
+      // send an A-record response for example.local
+      mdns.respond({
+        answers: [{
+          name: 'example.local',
+          type: 'A',
+          ttl: 300,
+          data: '192.168.1.5'
+        }]
+      })
+    }
   })
+})
+
+// lets query for an A-record for example.local
+mdns.query({
+  questions: [{
+    name: 'example.local',
+    type: 'A'
+  }]
+})
+
 }, 1000)
