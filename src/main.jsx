2912d0ab2f2e7f6289858fa1113894f2577903e1
Peter van Hardenberg <pvh@pvh.ca>
2018-08-31 14:22:33 -0700

    quieter formatting

--- src/main.jsx
+++ src/main.jsx
@@ -10,18 +10,20 @@ var DEFAULT_DISCOVERY = [
   'discovery2.datprotocol.com'
 ]
 
-var str = `hello world from a chrome app ${Math.round(Math.random() * 100)}`
+var apptype = `discover.js`
 
 var sw = swarm({utp: true, tcp: false, dht: false, dns: {server: DEFAULT_DISCOVERY, domain: DAT_DOMAIN}})
 
-sw.listen()
-sw.join('somekindoftest') // can be any id/name/hash
+sw.listen(0, () => console.log("we are on port ", sw.address().port))
+sw.join('peter') // can be any id/name/hash
 
-console.log('mine:', str)
+sw.on('peer', function(peer) {
+  console.log('peer discovered', peer.host, ':', peer.port)
+})
 
 sw.on('connection', function (connection) {
-  console.log('found + connected to peer', connection)
-  connection.write(str)
+  console.log('found + connected to peer')
+  connection.write(`hello from a ${apptype} on port ${sw.address().port}`)
   connection.on('data', function (data) {
     console.log('echo: ' + data)
   })
