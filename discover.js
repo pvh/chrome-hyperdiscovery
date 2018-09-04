7dd270756374e347a80dc11ed7db007508d02580
Orion Henry <orion.henry@gmail.com>
2018-09-04 10:36:53 -0400

    hypermerge

--- discover.js
+++ discover.js
@@ -1,30 +1,15 @@
 
 setTimeout( ()=> {
-  var swarm = require('discovery-swarm')
+  let Hypermerge = require('./src/hypermerge/index')
+//  let racf = require("random-access-chrome-file")
+//  var raf = require('random-access-file')
+  var ram = require('random-access-memory')
 
-var DAT_DOMAIN = 'dat.local'
-var DEFAULT_DISCOVERY = [
-  'discovery1.datprotocol.com',
-  'discovery2.datprotocol.com'
-]
 
-var apptype = `discover.js`
-
-var sw = swarm({utp: false, tcp: true, dht: false }) //, dns: {server: DEFAULT_DISCOVERY, domain: DAT_DOMAIN}})
-
-sw.listen(0, () => console.log("we are on port ", sw.address().port))
-sw.join('peter') // can be any id/name/hash
-
-sw.on('peer', function(peer) {
-  console.log('peer discovered', peer.host, ':', peer.port)
-})
-
-sw.on('connection', function (connection) {
-  console.log('found + connected to peer')
-  connection.write(`hello from a ${apptype} on port ${sw.address().port}`)
-  connection.on('data', function (data) {
-    console.log('echo: ' + data)
+  let hm = new Hypermerge({ storage: ram })
+  hm.once('ready', () => {
+    hm.joinSwarm()
+    cb()
   })
-})
 
 }, 1000)
--- src/main.jsx
+++ src/main.jsx
@@ -2,31 +2,16 @@
 process.hrtime = require('browser-process-hrtime')
 
 setTimeout( ()=> {
-  var swarm = require('discovery-swarm')
 
-var DAT_DOMAIN = 'dat.local'
-var DEFAULT_DISCOVERY = [
-  'discovery1.datprotocol.com',
-  'discovery2.datprotocol.com'
-]
+  let Hypermerge = require('./src/hypermerge/index')
+  let racf = require("random-access-chrome-file")
+//  var raf = require('random-access-file')
+//  var ram = require('random-access-memory')
 
-var apptype = `discover.js`
-
-var sw = swarm({utp: false, tcp: true, dht: false }) //, dns: {server: DEFAULT_DISCOVERY, domain: DAT_DOMAIN}})
-
-sw.listen(0, () => console.log("we are on port ", sw.address().port))
-sw.join('peter') // can be any id/name/hash
-
-sw.on('peer', function(peer) {
-  console.log('peer discovered', peer.host, ':', peer.port)
-})
-
-sw.on('connection', function (connection) {
-  console.log('found + connected to peer')
-  connection.write(`hello from a ${apptype} on port ${sw.address().port}`)
-  connection.on('data', function (data) {
-    console.log('echo: ' + data)
+  let hm = new Hypermerge({ storage: racf })
+  hm.once('ready', () => {
+    hm.joinSwarm()
+    cb()
   })
-})
 
 }, 1000)
