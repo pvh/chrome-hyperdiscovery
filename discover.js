b2464a141a8b79501fe61a491b7749e0f11ecac8
Orion Henry <orion.henry@gmail.com>
2018-09-04 12:14:28 -0400

    open doc

--- discover.js
+++ discover.js
@@ -1,15 +1,21 @@
 
 setTimeout( ()=> {
   let Hypermerge = require('./src/hypermerge/index')
-//  let racf = require("random-access-chrome-file")
-//  var raf = require('random-access-file')
-  var ram = require('random-access-memory')
+//  let storage = require("random-access-chrome-file")
+  var storage = require('random-access-file')
+//  var storage = require('random-access-memory')
 
 
-  let hm = new Hypermerge({ storage: ram })
+  let hm = new Hypermerge({ path: "derp", storage: storage })
   hm.once('ready', () => {
     hm.joinSwarm()
-    cb()
+    var doc = hm.create();
+    var id = hm.getId(doc);
+    var handle = hm.openHandle(id);
+//    var handle = hm.openHandle("cbfda8d7647862f250c6a627e1f48393e3df60a1825acd93fba15d575f6f3034");
+//    console.log("DOC",doc);
+//    console.log("ID",id);
+    console.log("HANDLE",handle);
   })
 
 }, 1000)
