046909c5c7c22f848fd631aeb3f4a9bb7d750241
Jeff Peterson <jeff@yak.sh>
2018-09-06 04:21:33 -0600

    Fix DebugObject for circular objs

--- src/components/DebugObject.jsx
+++ src/components/DebugObject.jsx
@@ -3,7 +3,7 @@ import DebugLogger from "./DebugLogger"
 
 export default class DebugObject extends React.Component {
   render() {
-    const { obj, map = JSON.stringify } = this.props
+    const { obj, map = inspect } = this.props
 
     return (
       <ul>
@@ -12,12 +12,29 @@ export default class DebugObject extends React.Component {
           : obj === undefined
             ? "undefined"
             : Object.entries(obj).map(([k, v]) => (
-                <li key={k}>
-                  <b>{k}</b>
-                  <DebugLogger tag={k} get={() => obj[k]} />: {map(v)}
-                </li>
-              ))}
+              <li key={k}>
+                <b>{k}</b>
+                <DebugLogger tag={k} get={() => obj[k]} />: {map(v)}
+              </li>
+            ))}
       </ul>
     )
+  }
+}
+
+function inspect(x) {
+  switch (typeof x) {
+    case "undefined": return "undefined"
+    case "string": return `"${x}"`
+    case "number": return String(x)
+    case "array": return `[${x.map()}]`
+    default:
+      if (x === null) {
+        return "null"
+      } else if (Array.isArray(x)) {
+        return `[${x.map(inspect).join(', ')}]`
+      } else {
+        return "Object"
+      }
   }
 }
