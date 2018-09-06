914b1407da7f1ace5b79233a46a2a065b078370b
Jeff Peterson <jeff@yak.sh>
2018-09-06 03:20:42 -0600

    Add mdns code

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,48 @@
 import * as React from "react"
 import { render } from "react-dom"
 import App from "./components/App"
+import racf from 'random-access-chrome-file'
+import dgram from 'chrome-dgram'
+import mdns from 'multicast-dns'
+import hd from 'hyperdiscovery'
+import hypercore from 'hypercore'
+import Hypermerge from "./hypermerge"
 
 process.hrtime = require("browser-process-hrtime")
 
 setTimeout(() => {
-  let Hypermerge = require("../src/hypermerge/index")
-  let racf = require("random-access-chrome-file")
-  //  var raf = require('random-access-file')
-  //  var ram = require('random-access-memory')
-
   let hm = new Hypermerge({ storage: racf })
   window.hm = hm
 
   render(<App hm={hm} />, window.app)
 
-  hm.once("ready", () => {
-    hm.joinSwarm()
+  const socket = dgram.createSocket('udp4')
+
+  socket.setMulticastTTL(255)
+  socket.setMulticastLoopback(true)
+
+  chrome.system.network.getNetworkInterfaces((ifaces) => {
+    socket.on('listening', function () {
+      for (let i = 0; i < ifaces.length; i++) {
+        if (ifaces[i].prefixLength == 24) {
+          socket.addMembership('224.0.0.251', ifaces[i].address)
+        }
+      }
+      run(socket)
+    })
+    socket.bind(5300)
   })
 }, 1000)
+
+
+
+function run(socket) {
+  const multicast = mdns({ socket, bind: false, port: 5300, multicast: false })
+
+  hm.ready.then(() => {
+    hm.joinSwarm({
+      dht: false,
+      dns: { multicast }
+    })
+  })
+}
